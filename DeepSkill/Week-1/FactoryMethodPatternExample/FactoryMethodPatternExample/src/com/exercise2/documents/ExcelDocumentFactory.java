cb74f051bc43e012cd191b7a6aee4667ff3c6901
Asha-1611 <ashakrishna1611@gmail.com>
2026-06-22 19:13:21 +0530

    Remove all broken folders from Week-1

--- DeepSkill/Week-1/FactoryMethodPatternExample/FactoryMethodPatternExample/src/com/exercise2/documents/ExcelDocumentFactory.java
+++
@@ -1,7 +0,0 @@
-package com.exercise2.documents;
-
-public class ExcelDocumentFactory extends DocumentFactory {
-    public Document createDocument() {
-        return new ExcelDocument();
-    }
-}
