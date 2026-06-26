7a9611c5f87114ebf7ef35a2730325879c0bc984
Asha-1611 <ashakrishna1611@gmail.com>
2026-06-26 21:17:23 +0530

    Added Week-2

---
+++ Week-2/Spring Core_Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+﻿package com.library.repository;
+
+public class BookRepository {
+    public void saveBook(String bookTitle) {
+        System.out.println("BookRepository: Saving book -> " + bookTitle);
+    }
+    public String findBook(String bookTitle) {
+        System.out.println("BookRepository: Finding book -> " + bookTitle);
+        return "Book found: " + bookTitle;
+    }
+}
